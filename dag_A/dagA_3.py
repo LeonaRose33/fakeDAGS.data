d8601d64059807bccc0f1d3b6bd753af42163513
leona rose <lrose@consoneo.com>
2023-10-30 16:30:41 +0100

    more dags

--- dag_A/dagA_3.py
+++ dag_A/dagA_3.py
@@ -1,6 +1,27 @@
-import datetime
+import logging
+import sys
+import traceback
+from datetime import datetime, timedelta
+from hashlib import sha256
 
+import pendulum
+import pytz
 from airflow import DAG
+from airflow.configuration import conf
+from airflow.decorators import task
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.utils.task_group import TaskGroup
+from pandas import DataFrame, isna, merge
+import numpy as np
+
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+log = logging.getLogger(__name__)
+sout_handler = logging.StreamHandler(sys.stdout)
+sout_handler.setFormatter(logging.Formatter(LOG_FORMAT))
+log.addHandler(sout_handler)
+
 from airflow.example_dags.subdags.subdag import subdag
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.subdag import SubDagOperator
