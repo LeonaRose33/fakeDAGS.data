733def77a2cbbe9fc0a3937202f4e90bb4f479ea
leona rose <lrose@consoneo.com>
2023-10-30 16:16:31 +0100

    more dags

--- dag_A/dagA_1.py
+++ dag_A/dagA_1.py
@@ -15,15 +15,16 @@ from airflow.utils.task_group import TaskGroup
 from pandas import DataFrame, isna, merge
 import numpy as np
 
-from utils import Utils
-from uuid import uuid4
-
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 log = logging.getLogger(__name__)
 sout_handler = logging.StreamHandler(sys.stdout)
 sout_handler.setFormatter(logging.Formatter(LOG_FORMAT))
 log.addHandler(sout_handler)
+
+################
+## PARAMETRES ##
+################
 
 sf_conn_id = "analytics"
 mysql_conn_id = "dossierc2e"
@@ -35,13 +36,30 @@ mysql_conn_id = "dossierc2e"
 with DAG(
     dag_id="dagA_1",
     catchup=False,
-    tags=["CONSONEO", "ETL"],
+    tags=["LEONA", "TEST"],
+    start_date=datetime(2022, 1, 1)
 ) as dag:
-    
-    
-    
 
+    @task(task_id="task1")
+    def task1():
+        log.info("starting task 1")
+
+    hello = task1()
+
+    @task(task_id="task2")
+    def task2():
+        log.info("starting task 2")
+
+    task2 = task2()
+
+    @task(task_id="task3")
+    def task3():
+        log.info("starting task 3")
+
+    task3 = task3()
 
     ######################
     ## TASKS DEFINITION ##
     ######################
+
+    hello >> [ task2, task3 ]
